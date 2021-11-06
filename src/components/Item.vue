831ad3dd83b8553df5773a8545e997aea8670cb4
Wendell Alves <wendellmccaulay@hotmail.com>
2021-11-06 20:24:00 -0300

    list tasks

---
+++ src/components/Item.vue
@@ -0,0 +1,58 @@
+<template>
+    <div id="item">
+        <ul>
+            <li v-for="task in lista" :key="task.key">
+                <span>{{ task.text }}</span>
+                <button>Concluir</button>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Item',
+    props: [
+        lista
+    ]
+}
+</script>
+
+<style scoped>
+    #item {
+        white-space: pre-wrap;
+        word-wrap: break-word;
+    }
+
+    ul {
+        list-style: none;
+        padding: 0;
+        margin-top: 20px;
+    }
+
+    ul li {
+        padding: 15px 0;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    li:not(:first-child) {
+        border-top: 1px solid #eee;
+    }
+
+    a {
+        color: #0D2636;
+    }
+
+    button {
+        cursor: pointer;
+        background: #ddd;
+        color: #000;
+        border: 0;
+        padding: 8px;
+        outline: none;
+        border-radius: 4px;
+    }
+</style>
--- src/components/Task.vue
+++ src/components/Task.vue
@@ -9,12 +9,19 @@
             <button type="submit">Adicionar</button>
         </form>
 
+        <Item :lista="tarefas"/>
+
     </div>
 </template>
 
 <script>
+import Item from './Item.vue';
+
 export default {
     name: 'Task',
+    components: {
+        Item
+    },
     data() {
         return {
             tarefa: '',
